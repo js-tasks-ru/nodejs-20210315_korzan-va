c8460a947b8404d683427e7cf67690c4cf21b814
Vyacheslav <korzan.va@mail.ru>
2021-03-21 13:22:14 +0800

    fix 1 task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,5 @@
 function sum(a, b) {
-  if (typeof(a) !== `number` || typeof(b) !== `number`) throw new TypeError();
+  if (typeof a !== `number` || typeof b !== `number`) throw new TypeError();
   return a + b;
 }
 
