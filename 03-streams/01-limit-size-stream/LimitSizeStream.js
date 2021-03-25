b444c0ecec5b3a8f45b99fea3f26a51299bf1528
Vyacheslav <korzan.va@mail.ru>
2021-03-25 23:35:07 +0800

    03 module / 01 limit size stream

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -2,12 +2,27 @@ const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
+  #limit = 0;
+
   constructor(options) {
     super(options);
+    this.#limit = options.limit;
+    this.totalSize = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    let error;
+    try {
+      this.totalSize += chunk.length;
+      // console.log('totalSize: ', this.totalSize);
 
+      if (this.totalSize > this.#limit) throw new LimitExceededError();
+
+    } catch (e) {
+      error = e;
+    }
+
+    callback(error, chunk);
   }
 }
 
