80ffecf90540c6964a445ef5d49da58d211989ae
Vyacheslav <korzan.va@mail.ru>
2021-04-21 17:40:10 +0500

    03-sessions

--- 08-authentication/03-sessions/controllers/me.js
+++ 08-authentication/03-sessions/controllers/me.js
@@ -1,4 +1,6 @@
 module.exports.me = async function me(ctx, next) {
+  console.log('ctx.user: ', ctx.user);
+  
   ctx.body = {
     email: ctx.user.email,
     displayName: ctx.user.displayName,
--- 08-authentication/03-sessions/libs/mustBeAuthenticated.js
+++ 08-authentication/03-sessions/libs/mustBeAuthenticated.js
@@ -1,3 +1,11 @@
+const { NotAuthorized } = require('../libs/errors');
+
+
 module.exports = function mustBeAuthenticated(ctx, next) {
+
+  if (!ctx.user) {
+    throw new NotAuthorized(`Пользователь не залогинен`);
+  }
+
   return next();
 };
